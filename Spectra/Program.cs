7a606b45451db85fa5e1af6c4f1ea937c9bc8e42
kpsafvan <safvankpmes@gmail.com>
2025-11-16 02:21:55 +0530

    Swagger support added

--- Spectra/Program.cs
+++ Spectra/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -12,6 +13,15 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Spectra v1"));
+    
+    // Redirect root path to Swagger UI
+    app.MapGet("/", context =>
+    {
+        context.Response.Redirect("/swagger", permanent: false);
+        return Task.CompletedTask;
+    });
 }
 
 app.UseHttpsRedirection();
